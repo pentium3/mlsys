33adfa195dc5f4e032413f571c34d80510c60634
taihui <lixx5027@umn.edu>
2019-04-22 16:21:24 -0500

    add web & mysql

---
+++ vps/benchmark/WEB_2/bench.py
@@ -0,0 +1,21 @@
+import numpy as np
+import os
+import time
+
+class Bench(object):
+    total_ptime = 0
+
+    def Run(self, ):
+        start_time = time.time()
+        os.chdir('./benchmark/htstress/')
+        os.system('./build.sh')
+        os.system('./htstress -n 100000 -c 100 -t 4 www.google.com')
+        end_time = time.time()
+        self.total_ptime = (int)(end_time - start_time)
+        print("This is Benchmark htstress: "+(str)(self.total_ptime))
+        return (self.total_ptime)
+
+    def sampleRun(self,):
+        print("This is Benchmark htstress")
+        return (10)
+
