bcb6b9bf8458c093c8bba91c13345a76f903dab9
pentium3 <celerond@msn.com>
2019-04-23 02:56:46 -0500

    .....

--- host/pretrain.py
+++ host/pretrain.py
@@ -11,7 +11,7 @@ import zmq
 import pickle
 
 
-benchlist=['FFMPEG2', 'FFMPEG1', '7ZBENCH2']
+benchlist=['7ZBENCH2', 'MYSQL_1', 'MYSQL_2', 'MYSQL_3', 'MYSQL_4', 'WEB', 'WEB_2', 'WEB_3']
 
 class SearchSpace():
     def ReadCfgFile(self, CfgFile):
