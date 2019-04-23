1e664acbfbacdadfd632f9450e69f3fb39de1941
Minjun Wu <higsyuhing@users.noreply.github.com>
2019-04-23 12:09:21 -0500

    Update bench.py

--- vps/benchmark/WEIGHTTP/bench.py
+++ vps/benchmark/WEIGHTTP/bench.py
@@ -8,7 +8,7 @@ class Bench(object):
     def Run(self, ):
         os.chdir('./benchmark/WEIGHTTP/')
         start_time = time.time()
-        os.system('./weighttp-master/weighttp -n 10000 -c 10 -t 2 -k -H "User-Agent: foo" http://www-users.cs.umn.edu/~kauffman/4061/schedule.html')
+        os.system('weighttp -n 10000 -c 10 -t 2 -k -H "User-Agent: foo" http://www-users.cs.umn.edu/~kauffman/4061/schedule.html')
         end_time = time.time()
         self.total_ptime = (int)(end_time - start_time)
         print("This is Benchmark WEIGHTTP: "+(str)(self.total_ptime))
