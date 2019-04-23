001b4b5175f95e4899d3ca56b6313c2e25dc594e
pentium3 <celerond@msn.com>
2019-04-23 16:23:03 -0500

    ...

--- vps/benchmark/MYSQL_4/bench.py
+++ vps/benchmark/MYSQL_4/bench.py
@@ -8,9 +8,9 @@ class Bench(object):
     def Run(self, ):
         start_time = time.time()
         os.chdir('./benchmark/MYSQL_4/')
-        os.system('sysbench oltp_read_write --db-driver=mysql --table-size=10000000  --mysql-host=localhost --mysql-db=hive --mysql-user=root --mysql-password=123456 prepare')
-        os.system('sysbench oltp_read_write --db-driver=mysql --table-size=10000000 --mysql-db=hive --mysql-user=root --mysql-password=123456 --max-requests=0 --num-threads=16 --oltp-reconnect-mode=random run')
-        os.system('sysbench oltp_read_write --db-driver=mysql --table-size=10000000  --mysql-host=localhost --mysql-db=hive --mysql-user=root --mysql-password=123456 cleanup')
+        os.system('sysbench oltp_read_write --db-driver=mysql --table-size=5000000  --mysql-host=localhost --mysql-db=hive --mysql-user=root --mysql-password=123456 prepare')
+        os.system('sysbench oltp_read_write --db-driver=mysql --table-size=5000000 --mysql-db=hive --mysql-user=root --mysql-password=123456 --max-requests=0 --num-threads=16 --oltp-reconnect-mode=random run')
+        os.system('sysbench oltp_read_write --db-driver=mysql --table-size=5000000  --mysql-host=localhost --mysql-db=hive --mysql-user=root --mysql-password=123456 cleanup')
         end_time = time.time()
         self.total_ptime = (int)(end_time - start_time)
         print("This is Benchmark MYSQL: "+(str)(self.total_ptime))
